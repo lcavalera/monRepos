b14b8340d27e023348ae6174d38398101692ec9a
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2023-04-24 13:49:30 -0400

    option 1 - ajout de la méthode Miauler

--- Tp2/Chat.cs
+++ Tp2/Chat.cs
@@ -5,13 +5,18 @@ using System;
 namepace H23_DevInfo
 {
     public class Chat
-    {
-        public string Nom { get; set; }
-        public int Age { get; set;}
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
 
-        public void Bondir()
-        {
-            Console.WriteLine($"{Nom} bondit!");
-        }
+    public void Bondir()
+    {
+        Console.WriteLine($"{Nom} bondit!");
     }
+
+    public void Miauler()
+    {
+        Console.WriteLine($"{Nom} miaule!");
+    }
+}
 }
